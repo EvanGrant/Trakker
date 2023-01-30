6f584554b73dffa9303d0854afa0a6fe736b7e4b
Noah Dubitzky <noahdubitzky2744@gmail.com>
2023-01-30 16:43:32 -0500

    Created Page and Test Push

---
+++ app/src/main/java/com/example/trakker/UserAccountPage.java
@@ -0,0 +1,14 @@
+package com.example.trakker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserAccountPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_account_page);
+    }
+}
