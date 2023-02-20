a09797ed54e06ac4c29032be58496cbdb5f3fa3e
Evan Grant <evan.grant@snhu.edu>
2023-02-19 21:15:52 -0500

    Added NavBar Ability

--- app/src/main/java/com/example/trakker/MainPage.java
+++ app/src/main/java/com/example/trakker/MainPage.java
@@ -1,8 +1,13 @@
 package com.example.trakker;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainPage extends AppCompatActivity {
 
@@ -10,5 +15,35 @@ public class MainPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+
+        // Initialize and assign variable
+        BottomNavigationView bottomNavigationView=findViewById(R.id.bottom_navigation);
+
+        // Set Home selected
+        bottomNavigationView.setSelectedItemId(R.id.MainPage);
+
+        // Perform item selected listener
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch(item.getItemId())
+                {
+                    case R.id.UserAccount:
+                        startActivity(new Intent(getApplicationContext(),UserAccountPage.class));
+                        overridePendingTransition(0,0);
+                        return true;
+
+                    case R.id.MainPage:
+                        return true;
+
+
+
+                }
+
+                return false;
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/trakker/UserAccountPage.java
+++ app/src/main/java/com/example/trakker/UserAccountPage.java
@@ -1,8 +1,13 @@
 package com.example.trakker;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class UserAccountPage extends AppCompatActivity {
 
@@ -10,5 +15,32 @@ public class UserAccountPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_account_page);
+
+        // Initialize and assign variable
+        BottomNavigationView bottomNavigationView=findViewById(R.id.bottom_navigation);
+
+        // Set Home selected
+        bottomNavigationView.setSelectedItemId(R.id.UserAccount);
+
+        // Perform item selected listener
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch(item.getItemId())
+                {
+                    case R.id.MainPage:
+                        startActivity(new Intent(getApplicationContext(),MainPage.class));
+                        overridePendingTransition(0,0);
+                        return true;
+
+
+                    case R.id.Dashboard:
+                        return true;
+
+                }
+                return false;
+            }
+        });
     }
 }
