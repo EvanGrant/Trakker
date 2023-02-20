436c658cbdfe664974e80fc993748d0898b52f77
StevenRSNHU <60667716+StevenRSNHU@users.noreply.github.com>
2023-02-20 15:45:28 -0500

    Redid list page structure

---
+++ app/src/main/java/com/example/trakker/ListAdapter.java
@@ -0,0 +1,54 @@
+package com.example.trakker;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+public class ListAdapter extends BaseAdapter {
+
+    Context context;
+
+    String ListUser[];
+
+    LayoutInflater inflator;
+
+
+
+
+
+    public ListAdapter(Context ctx, String [] UserList) {
+
+        this.context = ctx;
+        this.ListUser = UserList;
+        inflator =LayoutInflater.from(ctx);
+
+
+
+    }
+
+        @Override
+        public int getCount () {
+            return ListUser.length;
+        }
+
+        @Override
+        public Object getItem ( int i){
+            return null;
+        }
+
+        @Override
+        public long getItemId ( int i){
+            return 0;
+        }
+
+        @Override
+        public View getView ( int i, View convertView, ViewGroup viewGroup){
+
+            convertView= inflator.inflate(R.layout.activity_list_page, null);
+
+            return null;
+        }
+
+}
