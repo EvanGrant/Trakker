985e1841d960e9cfcd8b41c1368aa0dd32adffb2
Evan Grant <evan.grant@snhu.edu>
2023-02-27 11:21:55 -0500

    Added Horizontal Recycler View on Main Page. Filled with dummy data to get the general look, will be fixing with actual data once the mysql stuff works. Commit.

--- app/src/main/java/com/example/trakker/MainPage.java
+++ app/src/main/java/com/example/trakker/MainPage.java
@@ -2,19 +2,61 @@ package com.example.trakker;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.MenuItem;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainPage extends AppCompatActivity {
+public class MainPage extends AppCompatActivity implements MyRecyclerViewAdapter.ItemClickListener {
+
+    private MyRecyclerViewAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+
+        // data to populate the RecyclerView with
+        ArrayList<Integer> viewColors = new ArrayList<>();
+        viewColors.add(Color.BLUE);
+        viewColors.add(Color.YELLOW);
+        viewColors.add(Color.MAGENTA);
+        viewColors.add(Color.RED);
+        viewColors.add(Color.BLACK);
+
+        ArrayList<String> animalNames = new ArrayList<>();
+        animalNames.add("Horse");
+        animalNames.add("Cow");
+        animalNames.add("Camel");
+        animalNames.add("Sheep");
+        animalNames.add("Goat");
+
+        // set up the RecyclerView
+        RecyclerView recyclerView = findViewById(R.id.rvAnimals);
+        LinearLayoutManager horizontalLayoutManager
+                = new LinearLayoutManager(MainPage.this, LinearLayoutManager.HORIZONTAL, false);
+        recyclerView.setLayoutManager(horizontalLayoutManager);
+        adapter = new MyRecyclerViewAdapter(this, viewColors, animalNames);
+        adapter.setClickListener(this);
+        recyclerView.setAdapter(adapter);
 
         // Initialize and assign variable
         BottomNavigationView bottomNavigationView=findViewById(R.id.bottom_navigation);
@@ -46,4 +88,12 @@ public class MainPage extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    public void onItemClick(View view, int position) {
+        Toast.makeText(this, "You clicked " + adapter.getItem(position) + " on item position " + position, Toast.LENGTH_SHORT).show();
+    }
+
 }
+
+
