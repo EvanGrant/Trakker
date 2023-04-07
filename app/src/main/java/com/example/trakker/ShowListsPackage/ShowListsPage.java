5800248f44151d23b190998467c63aa63a5d5b80
Evan Grant <evan.grant@snhu.edu>
2023-04-06 23:15:57 -0400

    Added ShowListsPage

---
+++ app/src/main/java/com/example/trakker/ShowListsPackage/ShowListsPage.java
@@ -0,0 +1,16 @@
+package com.example.trakker.ShowListsPackage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.trakker.R;
+
+public class ShowListsPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_lists_page);
+    }
+}
